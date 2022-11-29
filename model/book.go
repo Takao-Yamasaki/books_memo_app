f2933f5d45b3dd45a96c8f62e6ce12b698be9c27
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-11-29 15:41:29 +0900

    型を修正

--- model/book.go
+++ model/book.go
@@ -1,7 +1,7 @@
 package model
 
 type Book struct {
-	Id      int46  `xorm:"pk autoincr int64" from:"id" json:"id"`
+	Id      int64  `xorm:"pk autoincr int64" from:"id" json:"id"`
 	Title   string `xorm:"varchar(40)" json:"title" from:"title"`
 	Content string `xorm:"varchar(40)" json:"content" from:"content"`
 }
