c62967cf59d5b5c9502506753093d54545def653
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-11-29 14:33:32 +0900

    DB処理の実装

--- service/book.go
+++ service/book.go
@@ -1,7 +1,43 @@
-package model
 
-type Book struct {
-	Id      int46  `xorm:"pk autoincr int64" from:"id" json:"id"`
-	Title   string `xorm:"varchar(40)" json:"title" from:"title"`
-	Content string `xorm:"varchar(40)" json:"content" from:"content"`
+package service
+
+import (
+	"books_memo_app/model"
+)
+
+// コントローラから振られたDB操作を引き受ける
+type BookService struct {}
+
+func (BookService) SetBook(book *model.Book) error {
+	_, err := DbEngine.Insert(book)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (BookService) GetBookList() []model.Book {
+	tests := make([]model.Book, 0)
+	err := DbEngine.Distinct("id", "title", "content").Limit(10, 0).Find(&tests)
+	if err != nil {
+		panic(err)
+	}
+	return tests
+}
+
+func (BookService) UpdateBook(newBook *model.Book) error {
+	_, err := DbEngine.Id(newBook.Id).Update(newBook)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (BookService) DeleteBook(id int) error {
+	book := new(model.Book)
+	_, err := DbEngine.Id(id).Delete(book)
+	if err != nil {
+		return err
+	}
+	return nil
 }
